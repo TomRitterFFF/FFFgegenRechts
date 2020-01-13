8e10538056229472d18c16e35a8ba8a4a09de4fa
TomRitterFFF <59840637+TomRitterFFF@users.noreply.github.com>
2020-01-13 19:11:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,20 +3,50 @@ var imageLoader = document.getElementById('imageLoader');
 var canvas = document.getElementById('imageCanvas');
 var ctx = canvas.getContext('2d');
 var bckg = new Image;
+var aspectRatio =1;
 bckg.setAttribute('crossorigin', 'anonymous');
 bckg.src ="https://i.imgur.com/ooRPEpR.png";
 
 function handleImage(e){
     var reader = new FileReader();
     reader.onload = function(event){
-        var img = new Image();
-        img.onload = function(){
-            canvas.width = 800;
-            canvas.height = 800;
-            ctx.drawImage(img, 90, 90, 620, 620);
-            ctx.drawImage(bckg, 0, 0, 800, 800);
-        }
-        img.src = event.target.result;
+        var inputImage = new Image();
+        inputImage.onload = function(){
+
+          // let's store the width and height of our image
+            const inputWidth = inputImage.naturalWidth;
+            const inputHeight = inputImage.naturalHeight;
+
+            // get the aspect ratio of the input image
+            const inputImageAspectRatio = inputWidth / inputHeight;
+
+            // if it's bigger than our target aspect ratio
+            let outputWidth = inputWidth;
+            let outputHeight = inputHeight;
+            if (inputImageAspectRatio > aspectRatio) {
+                outputWidth = inputHeight * aspectRatio;
+            } else if (inputImageAspectRatio < aspectRatio) {
+                outputHeight = inputHeight / aspectRatio;
+            }
+
+            // calculate the position to draw the image at
+            const outputX = (outputWidth - inputWidth) * .5;
+            const outputY = (outputHeight - inputHeight) * .5;
+
+            // create a canvas that will present the output image
+            const outputImage = document.createElement('canvas');
+
+            // set it to the same size as the image
+            outputImage.width = outputWidth;
+            outputImage.height = outputHeight;
+
+            // draw our image at position 0, 0 on the canvas
+            canvas.width = outputWidth;
+            canvas.height = outputHeight;
+            ctx.drawImage(inputImage, outputX, outputY);
+            ctx.drawImage(bckg, 0, 0, outputHeight, outputWidth);
+      }
+      inputImage.src = event.target.result;
     }
     reader.readAsDataURL(e.target.files[0]);
 }
@@ -27,4 +57,53 @@ function download_image(){
   link.download = "my-image.png";
   link.href = image;
   link.click();
+}
+function crop(url, aspectRatio) {
+
+    // we return a Promise that gets resolved with our canvas element
+    return new Promise(resolve => {
+
+        // this image will hold our source image data
+        const inputImage = new Image();
+
+        // we want to wait for our image to load
+        inputImage.onload = () => {
+
+            // let's store the width and height of our image
+            const inputWidth = inputImage.naturalWidth;
+            const inputHeight = inputImage.naturalHeight;
+
+            // get the aspect ratio of the input image
+            const inputImageAspectRatio = inputWidth / inputHeight;
+
+            // if it's bigger than our target aspect ratio
+            let outputWidth = inputWidth;
+            let outputHeight = inputHeight;
+            if (inputImageAspectRatio > aspectRatio) {
+                outputWidth = inputHeight * aspectRatio;
+            } else if (inputImageAspectRatio < aspectRatio) {
+                outputHeight = inputHeight / aspectRatio;
+            }
+
+            // calculate the position to draw the image at
+            const outputX = (outputWidth - inputWidth) * .5;
+            const outputY = (outputHeight - inputHeight) * .5;
+
+            // create a canvas that will present the output image
+            const outputImage = document.createElement('canvas');
+
+            // set it to the same size as the image
+            outputImage.width = outputWidth;
+            outputImage.height = outputHeight;
+
+            // draw our image at position 0, 0 on the canvas
+            const ctx = outputImage.getContext('2d');
+            ctx.drawImage(inputImage, outputX, outputY);
+            resolve(outputImage);
+        };
+
+        // start loading our image
+        inputImage.src = url;
+    })
+
 }
